c81c66427664266d0b26f57d29781c0fe2abf6c7
natallialemiasheuskaya <natkaber1995@gmail.com>
2019-10-03 14:24:04 +0300

    Initial Commit

---
+++ HoweWork1.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoweWork1.1
+//
+//  Created by Наташа Лемешевская on 10/3/19.
+//  Copyright © 2019 Наташа Лемешевская. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
